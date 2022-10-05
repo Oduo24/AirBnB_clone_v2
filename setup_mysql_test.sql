ebbdb8af74f52854a8af820d6a441c5c11f1dad8
Oduo24 <geraldoduo@gmail.com>
2022-10-05 11:57:42 +0300

    Added setup_mysql_test.sql

---
+++ setup_mysql_test.sql
@@ -0,0 +1,8 @@
+--Creates a db and user with permissions
+--The db hbnb_test_db
+--The user hbnb_test host localhost
+--The password for hbnb_test is hbnb_test_pwd
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON `hbnb_test`.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_test'@'localhost';
