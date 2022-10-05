db79908c8e980e4dd1f111499c64e38a5dcac39a
Oduo24 <geraldoduo@gmail.com>
2022-10-05 12:02:10 +0300

    Corrected FLUSH PRIVILEGES statement

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -3,6 +3,7 @@
 --The user hbnb_test host localhost
 --The password for hbnb_test is hbnb_test_pwd
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-GRANT ALL PRIVILEGES ON `hbnb_test`.* TO 'hbnb_test'@'localhost';
-GRANT SELECT ON `performance_schema`.* TO 'hbnb_test'@'localhost';
+CREATE USER IF NOT EXISTS `hbnb_test`@`localhost` IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON `hbnb_test`.* TO `hbnb_test`@`localhost`;
+GRANT SELECT ON `performance_schema`.* TO `hbnb_test`@`localhost`;
+FLUSH PRIVILEGES;
