de9ecd2f35ea3fd64b7ef33e4f5183bc046be68a
Oduo24 <geraldoduo@gmail.com>
2022-10-05 11:45:16 +0300

    Quick fix

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,7 +1,17 @@
---Creates a db, creates a user and gives privileges to the user
+-- Script that prepares a MySQL server for the project.
+-- A database hbnb_dev_db
+-- A new use hbnb_dev in localhost.
+-- The password hbnb_dev_pwd.
+-- The database hbnb_dev_db
+
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-DROP USER IF EXISTS 'hbnb_dev'@'localhost';
-CREATE USER 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
-GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
+CREATE USER
+    IF NOT EXISTS `hbnb_dev`@`localhost`
+    IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES
+   ON `hbnb_dev_db`.*
+   TO `hbnb_dev`@`localhost`;
+GRANT SELECT
+   ON `performance_schema`.*
+   TO `hbnb_dev`@`localhost`;
 FLUSH PRIVILEGES;
