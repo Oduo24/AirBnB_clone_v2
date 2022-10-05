baac6e813424a879f33c27be6b8c0980b34e3ab1
Oduo24 <geraldoduo@gmail.com>
2022-10-05 11:37:12 +0300

    Minor fix

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,10 +1,6 @@
 --Creates a db, creates a user and gives privileges to the user
-
---create db
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
---create user
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
---grant permissions
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost' WITH GRANT OPTION;
---grant permissions
-GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
