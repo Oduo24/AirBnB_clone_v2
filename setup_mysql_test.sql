2b1cc07d2719cda137c8878ba5663f4679c153fc
Oduo24 <geraldoduo@gmail.com>
2022-10-05 12:04:01 +0300

    Added more comment lines

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,9 +1,17 @@
---Creates a db and user with permissions
---The db hbnb_test_db
---The user hbnb_test host localhost
---The password for hbnb_test is hbnb_test_pwd
+-- Script that prepares a MySQL server for the project.
+-- A database hbnb_dev_db
+-- A new use hbnb_dev in localhost.
+-- The password hbnb_dev_pwd.
+-- The database hbnb_dev_db
+
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-CREATE USER IF NOT EXISTS `hbnb_test`@`localhost` IDENTIFIED BY 'hbnb_test_pwd';
-GRANT ALL PRIVILEGES ON `hbnb_test`.* TO `hbnb_test`@`localhost`;
-GRANT SELECT ON `performance_schema`.* TO `hbnb_test`@`localhost`;
+CREATE USER
+    IF NOT EXISTS `hbnb_test`@`localhost`
+    IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES
+   ON `hbnb_test_db`.*
+   TO `hbnb_test`@`localhost`;
+GRANT SELECT
+   ON `performance_schema`.*
+   TO `hbnb_test`@`localhost`;
 FLUSH PRIVILEGES;
