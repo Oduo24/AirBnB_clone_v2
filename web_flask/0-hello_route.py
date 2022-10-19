17401c6522fac1865bfd6434f75e8f88f9079b1b
Oduo24 <geraldoduo@gmail.com>
2022-10-19 15:17:02 +0300

    Added Shebang to the script

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,9 +1,12 @@
+#!/usr/bin/python3
+"""Starts a basic flask application"""
+
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def HelloHBNB():
+    """Defines root route response
+    """
     return 'Hello HBNB!'
-
-
