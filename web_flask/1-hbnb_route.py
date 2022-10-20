4be78fb59905ee30b7d6941dddc82aec73dff51a
Oduo24 <geraldoduo@gmail.com>
2022-10-20 11:04:43 +0300

    Quick fix

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -7,15 +7,14 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello_bnb():
-    """Reurns 'Hello HBNB on the root domain'
+    """Reurns 'Hello HBNB' upon visiting the root domain
     """
-    return 'Hello HBNB'
+    return 'Hello HBNB!'
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """Returns HBNB
+    """Returns HBNB upon visiting /hbnb
     """
     return 'HBNB'
-
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
