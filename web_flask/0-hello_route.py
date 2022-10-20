fd140cb0267a6846a2f1acaafe0236b03acb18c5
Oduo24 <geraldoduo@gmail.com>
2022-10-20 10:41:41 +0300

    Added FLASK_APP to environment variable list

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
 """Starts a basic flask application"""
 from flask import Flask
-import os
-
-os.environ['FLASK_APP'] = '0-hello_route.py'
-
 app = Flask(__name__)
 
 
@@ -14,6 +10,6 @@ def hello_hbnb():
     """
     return 'Hello HBNB!'
 
-if __name__ = '__main__':
+if __name__ == '__main__':
     """Main function"""
     app.run(host='0.0.0.0', port=5000)
