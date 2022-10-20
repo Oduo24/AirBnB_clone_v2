eb96a00deb8b8af918c33a55cf17e4d41c4d74fa
Oduo24 <geraldoduo@gmail.com>
2022-10-20 11:12:33 +0300

    Added string docs to function

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -5,16 +5,21 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
+
 @app.route('/', strict_slashes=False)
 def hello_bnb():
-    """Reurns 'Hello HBNB' upon visiting the root domain
+    """Displays 'Hello HBNB' upon visiting the root domain
     """
     return 'Hello HBNB!'
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """Returns HBNB upon visiting /hbnb
+    """Displays HBNB upon visiting /hbnb
     """
     return 'HBNB'
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
