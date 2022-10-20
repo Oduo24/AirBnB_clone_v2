73090c3ca23ae420a72ee71c3d92ae645ea0cc7c
Oduo24 <geraldoduo@gmail.com>
2022-10-20 10:56:29 +0300

    Added string docs

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -7,10 +7,14 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello_bnb():
+    """Reurns 'Hello HBNB on the root domain'
+    """
     return 'Hello HBNB'
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
+    """Returns HBNB
+    """
     return 'HBNB'
 
 if __name__ == '__main__':
